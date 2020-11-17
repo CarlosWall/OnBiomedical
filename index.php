9d61fbd55feacae388c26c331e8ed29f6d9d9307
CarlosWall <74607776+CarlosWall@users.noreply.github.com>
2020-11-17 13:14:17 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		
+	
+	</head>
+	<body>
+		<div id="cabeçalho">
+			<h1>OnBiomedical</h1>
+			<h2>Onde a ciência e a tecnologia se encontram</h2>
+		</div>
+		<div id="login">
+			<?php
+				echo "OnBiomedical Login";
+				echo "Enter username and password bellow";
+			?>
+	
+	</body>
+
+</html>
